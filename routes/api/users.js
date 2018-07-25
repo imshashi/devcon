68e2c535e432da7ddf4e380033f8f8182770dfc5
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-25 16:31:34 +0530

    add server side validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load input validation
+const validateRegisterInput = require('../../validations/register');
+
 // Load User model
 const User = require('../../models/User');
 
@@ -18,6 +21,13 @@ router.get('/test', (req, res) => res.json({ msg: "Users works" }));
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email })
     .then(user => {
       if (user) {
