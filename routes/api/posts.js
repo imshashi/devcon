d0b1d251171c0ff00b5c8875a9db994f88db97db
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-29 01:46:27 +0530

    add post like and unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -87,5 +87,64 @@ router.delete(
   }
 );
 
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+              return res.status(400).json({ already_liked: 'User already liked this post' });
+            }
+
+            // Add user id to likes array
+            post.likes.unshift({ user: req.user.id });
+
+            post.save()
+              .then(post => res.json(post))
+          })
+          .catch(error => res.status(404).json({ no_post_found: 'No post found' }))
+      })
+  }
+);
+
+// @route   POST api/posts/unlike/:id
+// @desc    Unlike post
+// @access  Private
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+              return res.status(400).json({ not_liked: 'You have not liked this post' });
+            }
+
+            // Get remove index
+            const removeIndex = post.likes
+              .map(item => item.user.toString())
+              .indexOf(req.user.id)
+
+            // Splice out of array
+            post.likes.splice(removeIndex, 1);
+
+            // Save
+            post.save().then(post => res.json(post));
+          })
+          .catch(error => res.status(404).json({ no_post_found: 'No post found' }))
+      })
+  }
+);
+
 
 module.exports = router;
