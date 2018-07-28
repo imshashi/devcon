f2f2eb474a3400b739dd6c9b31fcc69c175ba85a
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-28 22:09:40 +0530

    add get and delete post api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,8 @@ const passport = require('passport');
 
 // Load Post model
 const Post = require('../../models/Post');
+// Load Profile model
+const Profile = require('../../models/Profile');
 
 // Load input validation
 const validatePostInput = require('../../validations/post');
@@ -13,6 +15,25 @@ const validatePostInput = require('../../validations/post');
 // @desc    Tests posts route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: "Posts works" }));
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(error => res.status(404).json({ no_post_found: 'No posts found' }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(error => res.status(404).json({ no_post_found: 'No post found with that ID' }));
+});
 
 // @route   POST api/posts
 // @desc    Create post
@@ -39,5 +60,32 @@ router.post(
       .then(post => res.json(post));
   }
 );
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+
+            // Check for post owner
+            if(post.user.toString() != req.user.id) {
+              return res.status(401).json({ not_authorized: 'User is not authorized' });
+            }
+
+            // Delete
+            post.remove()
+              .then(() => res.json({ success: true }))
+              .catch(error => res.status(404).json({ no_post_found: 'No post found' }))
+          })
+      })
+  }
+);
+
 
 module.exports = router;
