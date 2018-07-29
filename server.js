2e08a6609d1b5619eb0f1b580a8fafa8414c25da
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-29 17:40:50 +0530

    add react for front-end to app

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
