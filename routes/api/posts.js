c3ecda5467174d51e9ad3548f51b499454c88f14
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-29 14:58:06 +0530

    add and remove comments routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -146,5 +146,69 @@ router.post(
   }
 );
 
+// @route   POST api/posts/:id/comments
+// @desc    Add comment to post
+// @access  Private
+router.post(
+  '/:id/comments',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        }
+
+        // Add to comments array
+        post.comments.unshift(newComment);
+
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(error => res.status(404).json({ no_post_found: 'No post found' }));
+  }
+);
+
+// @route   DELETE api/posts/:id/comments/:comment_id
+// @desc    Remove comment from post
+// @access  Private
+router.delete(
+  '/:id/comments/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    Post.findById(req.params.id)
+      .then(post => {
+
+        // Check if comment exists
+        if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+          return res.status(404).json({ no_comment_found: 'No comment found' })
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+              .map(item => item._id.toString())
+              .indexOf(req.params.comment_id)
+
+            // Splice comment out of array
+            post.comments.splice(removeIndex, 1);
+
+            // Save
+            post.save().then(post => res.json(post));
+
+      })
+      .catch(error => res.status(404).json({ no_post_found: 'No post found' }));
+  }
+);
 
 module.exports = router;
