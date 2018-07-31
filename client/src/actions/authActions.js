242df34909db31b52146f2c96f1b12b1e33ecd4f
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-31 16:08:29 +0530

    finish registration and add error reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,11 +1,20 @@
-import { TEST_DISPATCH } from './types';
+import { GET_ERRORS } from './types';
+import axios from 'axios';
 
 // Register
-export const registerUser = ((userData) => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  }
-});
+export const registerUser = (userData, history) => dispatch => {
+
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(error =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      })
+    );
+
+
+};
 
 
