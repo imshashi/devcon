d59e07d1bb3b091c9f577aea11ca5443a2ed936c
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-07-31 16:36:00 +0530

    redux - set login action, set current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,11 @@
-import { GET_ERRORS } from './types';
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register
 export const registerUser = (userData, history) => dispatch => {
-
   axios
     .post('/api/users/register', userData)
     .then(res => history.push('/login'))
@@ -13,8 +15,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: error.response.data
       })
     );
-
-
 };
 
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      // Get token from res
+      const { token } = res.data;
+      // Save token to localStorage
+      localStorage.setItem('jwtToken', token);
+      // Set token to auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentuser(decoded));
+    })
+    .catch(error =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      })
+    );
+}
 
+// Set logged in user
+export const setCurrentuser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
