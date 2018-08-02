69651971c000fc6e771f71c4932aa8a4bfc6154e
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-02 12:14:43 +0530

    setup private routes for app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
@@ -7,6 +7,7 @@ import { setCurrentUser, logoutUser } from './actions/authActions';
 import { clearCurrentProfile } from './actions/profileActions';
 
 import store from './store';
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -49,7 +50,9 @@ class App extends Component {
               <div className="container">
                 <Route exact path="/register" component={ Register } />
                 <Route exact path="/login" component={ Login } />
-                <Route exact path="/dashboard" component={ Dashboard } />
+                <Switch>
+                  <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+                </Switch>
               </div>
             <Footer />
           </div>
