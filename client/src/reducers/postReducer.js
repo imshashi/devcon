063027e423794b5be44d88f74bd6a3d294a6dd74
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-07 01:15:23 +0530

    implement delete post, like/unlike post features

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST, GET_POSTS, POST_LOADING } from '../actions/types';
+import { ADD_POST, GET_POSTS, DELETE_POST, POST_LOADING } from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -12,18 +12,23 @@ export default function(state=initialState, action) {
       return {
         ...state,
         loading: true
-      }
+      };
     case GET_POSTS:
       return {
         ...state,
         posts: action.payload,
         loading: false
-      }
+      };
     case ADD_POST:
       return {
         ...state,
         posts: [action.payload, ...state.posts]
-      }
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
+      };
     default:
       return state;
   }
