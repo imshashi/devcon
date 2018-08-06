e84cf57de36eab0a449cda262d80866dff5291ba
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-06 23:24:42 +0530

    Add posts and post form components with add post feature

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import NotFound from './components/not-found/NotFound';
 
 import './App.css';
@@ -73,6 +74,9 @@ class App extends Component {
                 </Switch>
                 <Switch>
                   <PrivateRoute exact path="/add-education" component={ AddEducation } />
+                </Switch>
+                <Switch>
+                  <PrivateRoute exact path="/feed" component={ Posts } />
                 </Switch>
                 <Route exact path="/not-found" component={ NotFound } />
               </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -19,6 +19,9 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/feed">Post Feed</Link>
+        </li>
+        <li className="nav-item">
           <Link className="nav-link" to="/dashboard">Dashboard</Link>
         </li>
         <li className="nav-item">
