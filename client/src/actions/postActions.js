50adae0ac72b5b034665b60bc3eace51297177ce
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-07 19:16:38 +0530

    show comments feed and allow to delete comment

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -6,12 +6,15 @@ import {
   GET_POSTS,
   POST_LOADING,
   DELETE_POST,
-  GET_ERRORS
+  GET_ERRORS,
+  CLEAR_ERRORS
 } from './types';
 
 
 // Add Post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
+
   axios
     .post('/api/posts', postData)
     .then(res =>
@@ -30,7 +33,7 @@ export const addPost = postData => dispatch => {
 
 // Get Posts
 export const getPosts = () => dispatch => {
-  dispatch(setPostLoading);
+  dispatch(setPostLoading());
   axios
     .get('/api/posts')
     .then(res =>
@@ -49,7 +52,7 @@ export const getPosts = () => dispatch => {
 
 // Get Post
 export const getPost = (id) => dispatch => {
-  dispatch(setPostLoading);
+  dispatch(setPostLoading());
   axios
     .get(`/api/posts/${id}`)
     .then(res =>
@@ -66,6 +69,26 @@ export const getPost = (id) => dispatch => {
     )
 }
 
+// Add Comment
+export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
+
+  axios
+    .post(`/api/posts/${postId}/comments`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(error =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      })
+    )
+}
+
 // Delete Post
 export const deletePost = id => dispatch => {
   axios
@@ -74,6 +97,24 @@ export const deletePost = id => dispatch => {
       dispatch({
         type: DELETE_POST,
         payload: id
+      })
+    )
+    .catch(error =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data
+      })
+    )
+}
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/${postId}/comments/${commentId}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
       })
     )
     .catch(error =>
@@ -115,5 +156,13 @@ export const removeLike = (id) => dispatch => {
 export const setPostLoading = () => {
   return {
     type: POST_LOADING
+  }
+}
+
+// Clear Errors
+
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   }
 }
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
+import CommentFeed from '../post/CommentFeed';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
@@ -21,6 +23,8 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={ post } showActions={ false } />
+          <CommentForm postId={ post._id } />
+          <CommentFeed postId={ post._id } comments={ post.comments } />
         </div>
       )
     }
